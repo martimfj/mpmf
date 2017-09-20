705cb209697075e8278a0b966b3f58360b6ff9ad
Marcelo Prado <marceloterreiroprado@gmail.com>
2017-09-20 11:52:17 -0300

    loop to save 12 steps

--- 5-DTMF-Encode/src/decoderDTMF.py
+++ 5-DTMF-Encode/src/decoderDTMF.py
@@ -23,34 +23,48 @@ fs = 44100
 duration = 5
 x = np.linspace(0, duration, fs*duration)
 
+recordingSteps = 12
+
 sd.default.samplerate = fs
 sd.default.channels = 1
-arquivo_audio = "./audio/recebido.wav"
+arquivo_audio = "./audio/recebido"
 
+def generateFilePath(fileName, counter):
+    return fileName + str(counter) + ".wav"
 
 def record_to_file(file, data, fs):
     sf.write(file, data, fs)
 
-audio = sd.rec(duration*fs)
-print ("Microfone gravando... ")
-sd.wait()
 
-y = audio[:,0]
-sd.play(y, fs)
-print ("Reproduzindo audio gravado...")
-sd.wait()
+def play_sound(data, fs):
+    print("Reproduzindo audio gravado...")
+    sd.play(data, fs)
+    sd.wait()
 
-print("-------------------------")
-print ("Salvando dados no arquivo :")
-print (" - {}".format(arquivo_audio))
-record_to_file(arquivo_audio, y, fs)
-print("Arquivo Salvo")
 
-plt.plot(x[500:1000], y[500:1000])
-plt.title('Sond Wave')
-plt.ylabel('Amplitude')
-plt.xlabel('Tempo')
-plt.axis('tight')
-plt.legend(loc='upper right')
-plt.show()
+counter = 0
+while(counter < recordingSteps):
+    counter += 1
+    audio = sd.rec(duration*fs)
+    print ("Microfone gravando... ")
+    sd.wait()
 
+    y = audio[:,0]
+    play_sound(y, fs)
+
+    print("-------------------------")
+    print ("Salvando dados no arquivo :")
+    print (" - {}".format(arquivo_audio))
+    record_to_file(generateFilePath(arquivo_audio, counter), y, fs)
+    print("Arquivo Salvo")
+
+    plt.plot(x[500:1000], y[500:1000])
+    plt.title('Sond Wave')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Tempo')
+    plt.axis('tight')
+    plt.legend(loc='upper right')
+    plt.show()
+    plt.clf()
+    plt.cla()
+    plt.close()

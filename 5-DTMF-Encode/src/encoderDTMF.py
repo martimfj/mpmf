061403de9ed384863d643ccdbca1aa3a341bb25c
Martim <martim.ferreira.jose@gmail.com>
2017-09-19 21:29:10 -0300

    Interface gráfica e encoderDTMF prontos

--- 5-DTMF-Encode/src/encoderDTMF.py
+++ 5-DTMF-Encode/src/encoderDTMF.py
@@ -12,60 +12,44 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import math
 
-class encoder(object):
-    def __init__(self):
-        self.periodo = 1
-        self.fs = 44100
-        self.duration = 50
+periodo = 1
+fs = 44100
+duration = 50
 
-    def getTone(self, tone):
-        DTMF = {1: (697, 1209),
-                2: (697, 1336),
-                3: (697, 1477),
-                4: (770, 1209),
-                5: (770, 1336),
-                6: (770, 1477),
-                7: (852, 1209),
-                8: (852, 1336),
-                9: (852, 1477),
-                "A": (941, 1209),
-                0: (941, 1336),
-                "H": (941, 1477),
-                } 
-        return(DTMF.get(tone))
+def getTone(tone):
+    DTMF = {1: (697, 1209),
+            2: (697, 1336),
+            3: (697, 1477),
+            4: (770, 1209),
+            5: (770, 1336),
+            6: (770, 1477),
+            7: (852, 1209),
+            8: (852, 1336),
+            9: (852, 1477),
+            "A": (941, 1209),
+            0: (941, 1336),
+            "H": (941, 1477),
+            } 
+    return(DTMF.get(tone))
 
-    def makeTone(self, tone):
-        T1 = self.createToneWave(self.getTone(1))
-        T2 = self.createToneWave(self.getTone(2)) 
-        T3 = self.createToneWave(self.getTone(3))
-        
-        T4 = self.createToneWave(self.getTone(4))
-        T5 = self.createToneWave(self.getTone(5)) 
-        T6 = self.createToneWave(self.getTone(6)) 
+def makeTone(tone):
+    Tone = createToneWave(getTone(tone))
 
-        T7 = self.createToneWave(self.getTone(7)) 
-        T8 = self.createToneWave(self.getTone(8)) 
-        T9 = self.createToneWave(self.getTone(9)) 
+    sd.play(Tone, fs)
+    print("Reproduzindo tom criado...")
+    sd.wait()
+    
+    print("Plotanto gráfico do tom criado...")
+    plotTone(Tone)
 
-        TA = self.createToneWave(self.getTone("A"))
-        T0 = self.createToneWave(self.getTone(0))
-        TH = self.createToneWave(self.getTone("H"))
+def createToneWave(tone):
+    lower, higher = tone
+    x = np.linspace(0, periodo, fs * periodo)
+    return np.sin(2 * math.pi * x * lower) + np.sin(2 * math.pi * x * higher)
 
-        sd.play(tone, self.fs)
-        print("Reproduzindo tom criado...")
-        sd.wait()
-        
-        print("Plotanto gráfico do tom criado...")
-        self.plotTone(tom)
-
-    def createToneWave(self, tone):
-        lower, higher = tone
-        x = np.linspace(0, self.periodo, self.fs * self.periodo)
-        return np.sin(2 * math.pi * x * lower) + np.sin(2 * math.pi * x * higher)
-
-    def plotTone(self,value):
-        plt.title('Sond Wave')
-        plt.ylabel('Values')
-        plt.plot(value, label='values')
-        plt.legend(loc='upper right')
-        plt.show
+def plotTone(value):
+    plt.title('Sond Wave')
+    plt.ylabel('Values')
+    plt.plot(value, label='values')
+    plt.legend(loc='upper right')
+    plt.show
--- 5-DTMF-Encode/src/interface.py
+++ 5-DTMF-Encode/src/interface.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import encoderDTMF
 
-class Main(object):
+class Main():
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("300x400")
-        self.window.title("Key Pad")
-        self.window.configure(background = 'white')
+        self.window.title("MPMF")
+        self.window.configure(background = 'black')
         self.window.resizable(False, False)
 
         self.window.rowconfigure(0, minsize = 100)
@@ -23,90 +23,90 @@ class Main(object):
 
         self.buttonOne = tk.Button(self.window, text = "1", height = height, width = width)
         self.buttonOne.grid(row = 0, column = 0,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone1())
+        self.buttonOne.configure(command = self.Tone1)
 
         self.buttonOne = tk.Button(self.window, text = "2", height = height, width = width)
         self.buttonOne.grid(row = 0, column =1,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone2())
+        self.buttonOne.configure(command = self.Tone2)
 
         self.buttonOne = tk.Button(self.window, text = "3", height = height, width = width)
         self.buttonOne.grid(row = 0, column = 2,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone3())
+        self.buttonOne.configure(command = self.Tone3)
 
         self.buttonOne = tk.Button(self.window, text = "4", height = height, width = width)
         self.buttonOne.grid(row = 1, column = 0,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone4())
+        self.buttonOne.configure(command = self.Tone4)
 
         self.buttonOne = tk.Button(self.window, text = "5", height = height, width = width)
         self.buttonOne.grid(row = 1, column = 1,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone5())
+        self.buttonOne.configure(command = self.Tone5)
 
         self.buttonOne = tk.Button(self.window, text = "6", height = height, width = width)
         self.buttonOne.grid(row = 1, column = 2,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone6())
+        self.buttonOne.configure(command = self.Tone6)
 
         self.buttonOne = tk.Button(self.window, text = "7", height = height, width = width)
         self.buttonOne.grid(row = 2, column = 0,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone7())
+        self.buttonOne.configure(command = self.Tone7)
         
         self.buttonOne = tk.Button(self.window, text = "8", height = height, width = width)
         self.buttonOne.grid(row = 2, column = 1,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone8())
+        self.buttonOne.configure(command = self.Tone8)
 
         self.buttonOne = tk.Button(self.window, text = "9", height = height, width = width)
         self.buttonOne.grid(row = 2, column = 2,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone9())
+        self.buttonOne.configure(command = self.Tone9)
         
         self.buttonOne = tk.Button(self.window, text = "*", height = height, width = width)
         self.buttonOne.grid(row = 3, column = 0,  sticky="nsew")
-        self.buttonOne.configure(command = self.ToneA())
+        self.buttonOne.configure(command = self.ToneA)
 
         self.buttonOne = tk.Button(self.window, text = "0", height = height, width = width)
         self.buttonOne.grid(row = 3, column = 1,  sticky="nsew")
-        self.buttonOne.configure(command = self.Tone0())
+        self.buttonOne.configure(command = self.Tone0)
 
         self.buttonOne = tk.Button(self.window, text = "#", height = height, width = width)
         self.buttonOne.grid(row = 3, column = 2,  sticky="nsew")
-        self.buttonOne.configure(command = self.ToneH())
-        
-    def Tone1(self):
-        encoderDTMF.encoder().makeTone("T1")
-
-    # def Tone2(self):
-    #     self.encoderDTMF.makeTone("T2")
-
-    # def Tone3(self):
-    #     encoderDTMF.makeTone("T3")
-
-    # def Tone4(self):
-    #     encoderDTMF.makeTone("T4")
-
-    # def Tone5(self):
-    #     encoderDTMF.makeTone("T5")
-
-    # def Tone6(self):
-    #     encoderDTMF.makeTone("T6")
-
-    # def Tone7(self):
-    #     encoderDTMF.makeTone("T7")
-
-    # def Tone8(self):
-    #     encoderDTMF.makeTone("T8")
-
-    # def Tone9(self):
-    #     encoderDTMF.makeTone("T9")
-
-    # def Tone0(self):
-    #     encoderDTMF.makeTone("T0")
-
-    # def ToneA(self):
-    #     encoderDTMF.makeTone("TA")
-
-    # def ToneH(self):
-    #     encoderDTMF.makeTone("TH")
-
+        self.buttonOne.configure(command = self.ToneH)
+    
     def iniciar(self):
         self.window.mainloop()
+
+    def Tone1(self):
+        encoderDTMF.makeTone(1)
+
+    def Tone2(self):
+        encoderDTMF.makeTone(2)
+
+    def Tone3(self):
+        encoderDTMF.makeTone(3)
+
+    def Tone4(self):
+        encoderDTMF.makeTone(3)
+
+    def Tone5(self):
+        encoderDTMF.makeTone(4)
+
+    def Tone6(self):
+        encoderDTMF.makeTone(6)
+
+    def Tone7(self):
+        encoderDTMF.makeTone(7)
+
+    def Tone8(self):
+        encoderDTMF.makeTone(8)
+
+    def Tone9(self):
+        encoderDTMF.makeTone(9)
+
+    def Tone0(self):
+        encoderDTMF.makeTone(0)
+
+    def ToneA(self):
+        encoderDTMF.makeTone("A")
+
+    def ToneH(self):
+        encoderDTMF.makeTone("H")
 
 app = Main()
 app.iniciar()

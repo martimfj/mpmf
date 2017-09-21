31401e081a2b92030f7ee6b2a5c45d601bbb5cf9
Marcelo Prado <marceloterreiroprado@gmail.com>
2017-09-21 15:10:48 -0300

    saves all received signals

--- 5-DTMF-Encode/src/decoderDTMF.py
+++ 5-DTMF-Encode/src/decoderDTMF.py
@@ -12,7 +12,7 @@ from path import path
 # https://python-sounddevice.readthedocs.io/en/0.3.8/
 
 fs = 44100
-duration = 1
+duration = 3
 x = np.linspace(0, duration, fs*duration)
 
 recordingSteps = 12
@@ -22,7 +22,7 @@ sd.default.channels = 1
 arquivo_audio = "./audio/recebido"
 
 def generateFilePath(fileName, counter):
-    return fileName + str(counter) + ".wav"
+    return "./audio/tom_ask.wav"#fileName + str(counter) + ".wav"
 
 def record_to_file(file, data, fs):
     sf.write(file, data, fs)
@@ -35,28 +35,24 @@ def play_sound(data, fs):
 
 
 counter = 0
-while(counter < recordingSteps):
-    counter += 1
-    audio = sd.rec(duration*fs)
-    print ("Microfone gravando... ")
-    sd.wait()
+# while(counter < recordingSteps):
+    # counter += 1
+audio = sd.rec(duration*fs)
+print ("Microfone gravando... ")
+sd.wait()
 
-    y = audio[:,0]
-    # play_sound(y, fs)
+y = audio[:,0]
+# play_sound(y, fs)
 
-    print("-------------------------")
-    print ("Salvando dados no arquivo :")
-    print (" - {}".format(arquivo_audio))
-    record_to_file(generateFilePath(arquivo_audio, counter), y, fs)
-    print("Arquivo Salvo")
+print("-------------------------")
+print ("Salvando dados no arquivo :")
+print (" - {}".format(arquivo_audio))
+record_to_file(generateFilePath(arquivo_audio, counter), y, fs)
+print("Arquivo Salvo")
 
-    plt.plot(x[500:1000], y[500:1000])
-    plt.title('Sound Wave')
-    plt.ylabel('Amplitude')
-    plt.xlabel('Tempo')
-    plt.axis('tight')
-    plt.legend(loc='upper right')
-    plt.show()
-    plt.clf()
-    plt.cla()
-    plt.close()
+plt.plot(x[500:1000], y[500:1000])
+plt.title('Sound Wave')
+plt.ylabel('Amplitude')
+plt.xlabel('Tempo')
+plt.axis('tight')
+plt.show()
